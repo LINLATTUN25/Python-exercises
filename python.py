5d154409d98ccb32286b1ae0dfb1df7f115766d9
LINLATUN25 <lin.l.htun@gmail.com>
2026-08-31 19:12:26 +0630

    Week 1 python exercises

---
+++ python.py
@@ -0,0 +1,12 @@
+def is_positive(n):
+    if n > 0:
+         print("Positive")
+    elif n < 0:
+         print("Negative")
+    else:
+         print("Zero")
+is_positive(-5)
+is_positive(0)
+is_positive(8)
+
+     
